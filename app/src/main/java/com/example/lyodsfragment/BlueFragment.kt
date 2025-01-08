b5343b4751812b1fc6f2060a5d28775369336906
kaleem <makanari.android@gmail.com>
2025-01-08 15:12:10 +0530

    dynamic frag

--- app/src/main/java/com/example/lyodsfragment/BlueFragment.kt
+++ app/src/main/java/com/example/lyodsfragment/BlueFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class BlueFragment : Fragment() {
@@ -14,6 +16,11 @@ class BlueFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         var myView = inflater.inflate(R.layout.blue_layout, container, false)
+       var resultTextView = myView.findViewById<TextView>(R.id.textViewResult)
+        var result = arguments?.getString("un")
+        Toast.makeText(activity, "data -- $result", Toast.LENGTH_LONG).show()
+        resultTextView.setText(result)
+
         return myView
     }
 
--- app/src/main/java/com/example/lyodsfragment/RedFragment.kt
+++ app/src/main/java/com/example/lyodsfragment/RedFragment.kt
@@ -4,16 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import kotlin.concurrent.fixedRateTimer
 
 class RedFragment : Fragment() {
-  // test
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         var myView = inflater.inflate(R.layout.red_layout, container,false)
+        var usernameEditText = myView.findViewById<EditText>(R.id.editTextTextUsername)
+        var submitButton = myView.findViewById<Button>(R.id.buttonSubmit)
+
+        submitButton.setOnClickListener {
+            var username = usernameEditText.text.toString()
+            var blueFrag = BlueFragment()
+            var bundle = Bundle()
+            bundle.putString("un", username)
+            blueFrag.arguments = bundle
+            parentFragmentManager?.beginTransaction()?.
+            replace(R.id.mainLayout, blueFrag)?.addToBackStack(null)?.commit()
+        }
         return myView
     }
 }
